da69bdea9ebf3056b622a7874c8d390df5e129dd
Collins Wakahenya <wakahenyacollins@gmail.com>
2023-08-30 19:36:24 +0300

    Done with assignment

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,14 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word.lower()
+
+    def match(self, word_list):
+        anagrams = []
+        for candidate in word_list:
+            if self.is_anagram(candidate.lower()):
+                anagrams.append(candidate)
+        return anagrams
+
+    def is_anagram(self, candidate):
+        return sorted(candidate) == sorted(self.word) and candidate != self.word
